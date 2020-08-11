9b6db46a0e722903da7be34d8f946412750b9071
Rizal Hilman <rizal@udacoding.com>
2020-08-11 13:13:54 +0700

    back to empty state

--- SwiftDelegateAndNotificationCentreExample/ViewController.swift
+++ SwiftDelegateAndNotificationCentreExample/ViewController.swift
@@ -8,38 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, ActionDelegate {
+class ViewController: UIViewController {
 
-    @IBOutlet weak var labelName: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     
-        NotificationCenter.default.addObserver(self, selector: #selector(dataChanged(notif:)), name: NOTIFICATION_DID_CHANGE_DATA, object: nil)
-    }
-    
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        print("Segue di panggil")
-//        let destinationVC = segue.destination as! ActionViewController
-//        destinationVC.delegate = self
-//    }
-    
-    @objc func dataChanged(notif: Notification) {
-        let text = notif.object as! String
-        labelName.text = text as String
-    }
-    
-    @IBAction func btnPressed(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "ActionViewController") as! ActionViewController
-        vc.delegate = self
-        self.present(vc, animated: true, completion: nil)
-    }
-    
-    func updateLabel(text: String) {
-        labelName.text = "Say \(text)"
-        print("Ini di print dari View Contoller Pertama! \(text)")
     }
 }
-
-let NOTIFICATION_DID_CHANGE_DATA = Notification.Name(rawValue: "NOTIFICATION_DID_CHANGE_DATA")
