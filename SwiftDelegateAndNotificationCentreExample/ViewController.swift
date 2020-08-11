fbfcb7a3467f193c08295bf416e1d93c42206555
Rizal Hilman <rizal@udacoding.com>
2020-08-11 08:50:49 +0700

    Initial commits

---
+++ SwiftDelegateAndNotificationCentreExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftDelegateAndNotificationCentreExample
+//
+//  Created by Rizal Hilman on 11/08/20.
+//  Copyright © 2020 Rizal Hilman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
