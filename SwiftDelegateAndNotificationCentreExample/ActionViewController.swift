0018110f6116a0c6f24230959fb34715f1bfaf10
Rizal Hilman <rizal@udacoding.com>
2020-08-11 10:26:58 +0700

    Adding new view controller

---
+++ SwiftDelegateAndNotificationCentreExample/ActionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ActionViewController.swift
+//  SwiftDelegateAndNotificationCentreExample
+//
+//  Created by Rizal Hilman on 11/08/20.
+//  Copyright © 2020 Rizal Hilman. All rights reserved.
+//
+
+import UIKit
+
+class ActionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
